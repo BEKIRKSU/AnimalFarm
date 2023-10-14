ef6c9a6049fc2278b16755b189f4a61d2c1ef8f0
BEKIRKSU <bekirkscu@gmail.com>
2023-10-14 10:29:21 +0100

    Home page being made for volunteer+manager

---
+++ src/Home.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import "./Home.css";
+
+function Home() {
+  const navigate = useNavigate();
+  const handleGetStartedClick = () => {
+    
+    navigate('/platform');
+  };
+
+ const handleDemoClick = () => {
+    navigate('/demo');
+};
+
+    return (
+        <div className="homePage">
+            <main className="content">
+              <div className="typewriter">
+                <h1>Welcome to MM-Code</h1>
+                <p>Embark on a journey of deep practice with us, overcoming initial challenges to attain coding expertise.</p>
+                </div>
+               <div className="button-group">
+          <button onClick={handleGetStartedClick} className="journey-on-button">
+            Volunteer
+          </button>
+          <button onClick={handleDemoClick} className="demo-button">Manager</button>
+        </div>
+            </main>
+        </div>
+    );
+}
+
+
+export default Home;
