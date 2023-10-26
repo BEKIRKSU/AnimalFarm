a44bbd0b13d4d460c0ad7b25c17d28589c4e0749
BEKIRKSU <bekirkscu@gmail.com>
2023-10-26 17:32:48 +0100

    Bookings are now viewable by the manager

--- src/UserBooking.jsx
+++ src/UserBooking.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import './UserBooking.css';
-import { computeHeadingLevel } from "@testing-library/react";
 
 function UserBooking({ data }) {
-  
-  console.log(data)
     return (
       
-      <div className="user-booking-container">
-        <p><strong>Name:</strong> {data.name}</p>
-        <p><strong>Email:</strong> {data.email}</p>
-        <p><strong>Session:</strong> {data.session}</p>
-        <p><strong>Date:</strong> {new Date(data.date).toLocaleDateString()}</p>
+      <div>
+         {data.map((booking, index) => (
+        <div key={index} className="user-booking-container">
+        <p><strong>Name:</strong> {booking.name}</p>
+        <p><strong>Email:</strong> {booking.email}</p>
+        <p><strong>Session:</strong> {booking.session}</p>
+        <p><strong>Date:</strong> {new Date(booking.date).toLocaleDateString()}</p>
       </div>
-    );
-}
+     ))}
+     </div>
+   );
+ }
 
 export default UserBooking;
 
