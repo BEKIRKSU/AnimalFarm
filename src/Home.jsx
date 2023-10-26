12de94322a25b1c5fe65a01259bd2e910ff42238
BEKIRKSU <bekirkscu@gmail.com>
2023-10-26 20:23:39 +0100

    Home page buttons sorted

--- src/Home.jsx
+++ src/Home.jsx
@@ -18,7 +18,7 @@ function Home() {
         <div className="homePage">
             <main className="content">
               <div className="fade-in">
-                <h1>Welcome to Bekir's Citflik.</h1>
+                <h1>Welcome to Bekir's Ciftlik.</h1>
                 </div>
                <div className="button-group">
           <button onClick={handleVolunteerLoginClick} className="volunteer-login-button">
