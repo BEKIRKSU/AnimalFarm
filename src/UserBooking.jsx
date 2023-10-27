6d4b5c2ff4f12dd07e6ed28b443a7e5c2c88c2c6
BEKIRKSU <bekirkscu@gmail.com>
2023-10-27 17:43:23 +0100

    The data is now in order

--- src/UserBooking.jsx
+++ src/UserBooking.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import './UserBooking.css';
 
 function UserBooking({ data }) {
+  const sortedData = [...data].sort((a, b) => new Date(b.date) - new Date(a.date));
     return (
       
       <div>
         <h1 className="bookingsTitle">Volunteer Bookings</h1>
-         {data.map((booking, index) => (
+         {sortedData.map((booking, index) => (
         <div key={index} className="user-booking-container">
         <p><strong>Name:</strong> {booking.name}</p>
         <p><strong>Email:</strong> {booking.email}</p>
