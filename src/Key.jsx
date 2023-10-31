23531b72197aaa5f5f71f8a7b437890e9ee06858
BEKIRKSU <bekirkscu@gmail.com>
2023-10-31 03:31:29 +0000

    new key list added

--- src/Key.jsx
+++ src/Key.jsx
@@ -5,6 +5,10 @@ const Key = () => {
   return (
     <ul className="key-container">
         <h3>Time of day booked:</h3>
+        <li className="key-item">
+        <div className="key-color white"></div>
+        <span>Morning & Evening</span>
+      </li>
       <li className="key-item">
         <div className="key-color morning"></div>
         <span>Morning Only</span>
